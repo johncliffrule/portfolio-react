faccf0e5ecc0cccccab8761ebeec975e0aaec9c5
John Clifford <john.clifford.ext@bayer.com>
2021-06-04 11:27:17 +0530

    Installed Material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
+import ParentComponent from './components/index';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React from John Clifford
-        </a>
-      </header>
+      <ParentComponent />
     </div>
   );
 }
